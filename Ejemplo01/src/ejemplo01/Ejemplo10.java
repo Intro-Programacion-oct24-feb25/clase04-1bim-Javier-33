7a6b8a10a0e17dd2030bfdf14a9d014846781114
JavierVinan <javinan5@utpl.edu.ec>
2024-10-30 11:54:30 -0500

    cambios bruscos

--- Ejemplo01/src/ejemplo01/Ejemplo10.java
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -11,12 +11,12 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo10 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "Javier Alejandro";
+        String apellidoEstudiante = "Vinan Uchuary";
+        int edad = 20;
         
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
@@ -26,7 +26,7 @@ public class Ejemplo05 {
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);   
+        //System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante+"\n\t\t"+edad);   
     }
 }
